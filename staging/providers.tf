0096ecd9e1222f9c32f5bd40a9cf6981f58853df
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2025-09-23 09:55:42 +1000

    initial deployment

--- staging/providers.tf
+++ staging/providers.tf
@@ -1,10 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.114" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.114"
+    }
   }
-  # local state is fine because apply + destroy run in the same job
-  backend "local" {}
 }
-
-provider "azurerm" { features {} }
+# The empty features block is REQUIRED for azurerm
+provider "azurerm" {
+  features {}
+}
